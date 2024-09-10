d315827f8397a4c0dd96cf6adc7145b698f693f9
skrollbrad <cliquebenito@mail.ru>
2024-09-10 14:33:11 +0300

    params change

--- models.go
+++ models.go
@@ -15,7 +15,6 @@ type TransactionByMasterchainBlockParams struct {
 	Offset int    `json:"offset"`
 	Sort   string `json:"sort"`
 }
-
 type TransactionByMasterChainBlockStruct struct {
 	Transactions []struct {
 		Account       string `json:"account"`
@@ -27,67 +26,58 @@ type TransactionByMasterChainBlockStruct struct {
 		TotalFees     string `json:"total_fees"`
 		PrevTransHash string `json:"prev_trans_hash"`
 		PrevTransLt   string `json:"prev_trans_lt"`
-		Description   string `json:"description"`
-		BlockRef      struct {
+		Description   struct {
+			Type   string `json:"type"`
+			Action struct {
+				Valid       bool `json:"valid"`
+				Success     bool `json:"success"`
+				NoFunds     bool `json:"no_funds"`
+				ResultCode  int  `json:"result_code"`
+				TotActions  int  `json:"tot_actions"`
+				MsgsCreated int  `json:"msgs_created"`
+				SpecActions int  `json:"spec_actions"`
+				TotMsgSize  struct {
+					Bits  string `json:"bits"`
+					Cells string `json:"cells"`
+				} `json:"tot_msg_size"`
+				StatusChange   string `json:"status_change"`
+				SkippedActions int    `json:"skipped_actions"`
+				ActionListHash string `json:"action_list_hash"`
+			} `json:"action"`
+			Aborted   bool `json:"aborted"`
+			IsTock    bool `json:"is_tock"`
+			Destroyed bool `json:"destroyed"`
+			ComputePh struct {
+				Mode             int    `json:"mode"`
+				Type             string `json:"type"`
+				Success          bool   `json:"success"`
+				GasFees          string `json:"gas_fees"`
+				GasUsed          string `json:"gas_used"`
+				VMSteps          int    `json:"vm_steps"`
+				ExitCode         int    `json:"exit_code"`
+				GasLimit         string `json:"gas_limit"`
+				MsgStateUsed     bool   `json:"msg_state_used"`
+				AccountActivated bool   `json:"account_activated"`
+				VMInitStateHash  string `json:"vm_init_state_hash"`
+				VMFinalStateHash string `json:"vm_final_state_hash"`
+			} `json:"compute_ph"`
+			StoragePh struct {
+				StatusChange         string `json:"status_change"`
+				StorageFeesCollected string `json:"storage_fees_collected"`
+			} `json:"storage_ph"`
+		} `json:"description"`
+		BlockRef struct {
 			Workchain int    `json:"workchain"`
 			Shard     string `json:"shard"`
 			Seqno     int    `json:"seqno"`
 		} `json:"block_ref"`
-		InMsg struct {
-			Hash           string `json:"hash"`
-			Source         string `json:"source"`
-			Destination    string `json:"destination"`
-			Value          string `json:"value"`
-			FwdFee         string `json:"fwd_fee"`
-			IhrFee         string `json:"ihr_fee"`
-			CreatedLt      string `json:"created_lt"`
-			CreatedAt      string `json:"created_at"`
-			Opcode         string `json:"opcode"`
-			IhrDisabled    bool   `json:"ihr_disabled"`
-			Bounce         bool   `json:"bounce"`
-			Bounced        bool   `json:"bounced"`
-			ImportFee      string `json:"import_fee"`
-			MessageContent struct {
-				Hash    string `json:"hash"`
-				Body    string `json:"body"`
-				Decoded struct {
-				} `json:"decoded"`
-			} `json:"message_content"`
-			InitState struct {
-				Hash string `json:"hash"`
-				Body string `json:"body"`
-			} `json:"init_state"`
-		} `json:"in_msg"`
-		OutMsgs []struct {
-			Hash           string `json:"hash"`
-			Source         string `json:"source"`
-			Destination    string `json:"destination"`
-			Value          string `json:"value"`
-			FwdFee         string `json:"fwd_fee"`
-			IhrFee         string `json:"ihr_fee"`
-			CreatedLt      string `json:"created_lt"`
-			CreatedAt      string `json:"created_at"`
-			Opcode         string `json:"opcode"`
-			IhrDisabled    bool   `json:"ihr_disabled"`
-			Bounce         bool   `json:"bounce"`
-			Bounced        bool   `json:"bounced"`
-			ImportFee      string `json:"import_fee"`
-			MessageContent struct {
-				Hash    string `json:"hash"`
-				Body    string `json:"body"`
-				Decoded struct {
-				} `json:"decoded"`
-			} `json:"message_content"`
-			InitState struct {
-				Hash string `json:"hash"`
-				Body string `json:"body"`
-			} `json:"init_state"`
-		} `json:"out_msgs"`
+		InMsg              any   `json:"in_msg"`
+		OutMsgs            []any `json:"out_msgs"`
 		AccountStateBefore struct {
 			Hash          string `json:"hash"`
 			Balance       string `json:"balance"`
 			AccountStatus string `json:"account_status"`
-			FrozenHash    string `json:"frozen_hash"`
+			FrozenHash    any    `json:"frozen_hash"`
 			CodeHash      string `json:"code_hash"`
 			DataHash      string `json:"data_hash"`
 		} `json:"account_state_before"`
@@ -95,7 +85,7 @@ type TransactionByMasterChainBlockStruct struct {
 			Hash          string `json:"hash"`
 			Balance       string `json:"balance"`
 			AccountStatus string `json:"account_status"`
-			FrozenHash    string `json:"frozen_hash"`
+			FrozenHash    any    `json:"frozen_hash"`
 			CodeHash      string `json:"code_hash"`
 			DataHash      string `json:"data_hash"`
 		} `json:"account_state_after"`
@@ -103,6 +93,94 @@ type TransactionByMasterChainBlockStruct struct {
 	} `json:"transactions"`
 	AddressBookResponse `json:"address_book"`
 }
+
+//	type TransactionByMasterChainBlockStruct struct {
+//		Transactions []struct {
+//			Account       string `json:"account"`
+//			Hash          string `json:"hash"`
+//			Lt            string `json:"lt"`
+//			Now           int    `json:"now"`
+//			OrigStatus    string `json:"orig_status"`
+//			EndStatus     string `json:"end_status"`
+//			TotalFees     string `json:"total_fees"`
+//			PrevTransHash string `json:"prev_trans_hash"`
+//			PrevTransLt   string `json:"prev_trans_lt"`
+//			Description   string `json:"description"`
+//			BlockRef      struct {
+//				Workchain int    `json:"workchain"`
+//				Shard     string `json:"shard"`
+//				Seqno     int    `json:"seqno"`
+//			} `json:"block_ref"`
+//			InMsg struct {
+//				Hash           string `json:"hash"`
+//				Source         string `json:"source"`
+//				Destination    string `json:"destination"`
+//				Value          string `json:"value"`
+//				FwdFee         string `json:"fwd_fee"`
+//				IhrFee         string `json:"ihr_fee"`
+//				CreatedLt      string `json:"created_lt"`
+//				CreatedAt      string `json:"created_at"`
+//				Opcode         string `json:"opcode"`
+//				IhrDisabled    bool   `json:"ihr_disabled"`
+//				Bounce         bool   `json:"bounce"`
+//				Bounced        bool   `json:"bounced"`
+//				ImportFee      string `json:"import_fee"`
+//				MessageContent struct {
+//					Hash    string `json:"hash"`
+//					Body    string `json:"body"`
+//					Decoded struct {
+//					} `json:"decoded"`
+//				} `json:"message_content"`
+//				InitState struct {
+//					Hash string `json:"hash"`
+//					Body string `json:"body"`
+//				} `json:"init_state"`
+//			} `json:"in_msg"`
+//			OutMsgs []struct {
+//				Hash           string `json:"hash"`
+//				Source         string `json:"source"`
+//				Destination    string `json:"destination"`
+//				Value          string `json:"value"`
+//				FwdFee         string `json:"fwd_fee"`
+//				IhrFee         string `json:"ihr_fee"`
+//				CreatedLt      string `json:"created_lt"`
+//				CreatedAt      string `json:"created_at"`
+//				Opcode         string `json:"opcode"`
+//				IhrDisabled    bool   `json:"ihr_disabled"`
+//				Bounce         bool   `json:"bounce"`
+//				Bounced        bool   `json:"bounced"`
+//				ImportFee      string `json:"import_fee"`
+//				MessageContent struct {
+//					Hash    string `json:"hash"`
+//					Body    string `json:"body"`
+//					Decoded struct {
+//					} `json:"decoded"`
+//				} `json:"message_content"`
+//				InitState struct {
+//					Hash string `json:"hash"`
+//					Body string `json:"body"`
+//				} `json:"init_state"`
+//			} `json:"out_msgs"`
+//			AccountStateBefore struct {
+//				Hash          string `json:"hash"`
+//				Balance       string `json:"balance"`
+//				AccountStatus string `json:"account_status"`
+//				FrozenHash    string `json:"frozen_hash"`
+//				CodeHash      string `json:"code_hash"`
+//				DataHash      string `json:"data_hash"`
+//			} `json:"account_state_before"`
+//			AccountStateAfter struct {
+//				Hash          string `json:"hash"`
+//				Balance       string `json:"balance"`
+//				AccountStatus string `json:"account_status"`
+//				FrozenHash    string `json:"frozen_hash"`
+//				CodeHash      string `json:"code_hash"`
+//				DataHash      string `json:"data_hash"`
+//			} `json:"account_state_after"`
+//			McBlockSeqno int `json:"mc_block_seqno"`
+//		} `json:"transactions"`
+//		AddressBookResponse `json:"address_book"`
+//	}
 type TransactionStruct struct {
 	Transactions []struct {
 		Account       string `json:"account"`
