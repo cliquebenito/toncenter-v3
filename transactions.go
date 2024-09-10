cc5a6b8d5b094984bc35a32fe026ee3ab1aac7bd
skrollbrad <cliquebenito@mail.ru>
2024-09-10 12:24:50 +0300

    tx

--- transactions.go
+++ transactions.go
@@ -6,6 +6,7 @@ import (
 
 type Transactioner interface {
 	Transactions(params TransactionsParams) (TransactionStruct, error)
+	TransactionByMasterchainBlock(params TransactionByMasterchainBlockParams) (TransactionByMasterChainBlockStruct, error)
 }
 type Transaction struct {
 	client Client
